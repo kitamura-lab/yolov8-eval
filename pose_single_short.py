1e24ccbafb6cf8186c1a1dd0be6657f75ef50fd0
kitamura-lab <ykitamura@kwansei.ac.jp>
2024-02-10 16:40:11 +0900

    2 video

--- pose_single_short.py
+++ pose_single_short.py
@@ -1,7 +1,8 @@
 from pose import Pose
 from clock import Clock
 
-video_path = "single_short.mp4"
+# video_path = "single_short.mp4"
+video_path = "single_long.mp4"
 
 clock = Clock()
 clock.start()
