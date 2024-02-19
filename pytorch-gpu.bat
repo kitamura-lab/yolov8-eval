9abf4a1ef3195df03b17b1a0468decec98a93f7f
kitamura-lab <ykitamura@kwansei.ac.jp>
2024-02-19 15:28:02 +0900

    pytorchのインストール

--- pytorch-gpu.bat
+++ pytorch-gpu.bat
@@ -1,4 +1,4 @@
 pip install ultralytics
 pip uninstall torch torchvision
-pip install torch==2.2.0 torchvision --extra-index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu121
 python check_gpu.py
